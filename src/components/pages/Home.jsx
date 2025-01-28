a68c9e8725f34d72f82eaae51622b222f9650461
Manuel Cau <caumanuel10@gmail.com>
2025-01-28 02:16:21 +0100

    header home complete

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,21 @@
+import homeVideo from "../../assets/banners/homeVideo.mp4";
+import MAP from '../../assets/Background/sfondo-map.svg'
+import logoHoley from '../../assets/SVG/Logos/HOLEY-cdd-TRASP-WHITE.svg'
+
 export function Home() {
   return (
     <div className="backgroundMap">
-     {/*  <div>
-      <iframe
-        className="videoHeader"
-        src="https://www.youtube.com/embed/6zB29pvmn9s?autoplay=1&loop=1&mute=1&playlist=6zB29pvmn9s&controls=0&showinfo=0&modestbranding=1"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        title="Background Video"
-      ></iframe></div> */}
+      <div className="homeHeader">
+        <img src={logoHoley} alt="cast,dream,discover" className="mainLogo"/>
+        <video
+          src={homeVideo}
+          className="videoHeader"
+          autoPlay
+          loop
+          muted
+        ></video>
+      </div>
+      <img src={MAP} alt="map" />
     </div>
   );
 }
