5bded69e9de44904f0dd4c1b96589e84f4b58c35
Manuel Cau <caumanuel10@gmail.com>
2025-02-12 16:51:27 +0100

    added culture pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import { About } from "./components/pages/About";
 import { Lures } from "./components/pages/Lures";
 import { Design } from "./components/pages/Design";
 import { Culture } from "./components/pages/Culture";
+import { CatchAndRelease } from "./components/pages/CatchAndRelease";
+import { Depth } from "./components/pages/Depth";
 function App() {
   return (
     <>
@@ -20,6 +22,8 @@ function App() {
           <Route path="/Design" element={<Design />} />
           <Route path="/Culture" element={<Culture />} />
           <Route path="/Contact" element={<Contact />} />
+          <Route path="/CatchAndRelease" element={<CatchAndRelease />} />
+          <Route path="/Depth" element={<Depth />} />
         </Routes>
       </div>
 
